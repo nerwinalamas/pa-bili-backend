869fd3b43d582962513fd50242d19557e32777ef
nerwinalamas <nerwinalamas@gmail.com>
2024-12-15 20:17:46 +0800

    refactor: add contact routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const connectToMongoDB = require("./config/db.js");
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
+const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -24,6 +25,7 @@ connectToMongoDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/contact", contactRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
