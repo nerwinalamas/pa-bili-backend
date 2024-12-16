0b50523f4a2ac2051477f6363662ea1958b677c8
nerwinalamas <nerwinalamas@gmail.com>
2024-12-16 22:07:40 +0800

    refactor: add order routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const contactRoutes = require("./routes/contactRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -26,6 +27,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/contact", contactRoutes);
+app.use("/api/order", orderRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
