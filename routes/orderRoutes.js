2a3776d123abeb7d1ecb8fa6b8e2ce1ea5f78494
nerwinalamas <nerwinalamas@gmail.com>
2024-12-17 22:05:08 +0800

    refactor: add new routes for get all orders and update order status

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
-const { createOrder, getOrderById, getUserOrders } = require("../controllers/orderController");
+const adminMiddleware = require("../middleware/adminMiddleware");
+const {
+    createOrder,
+    getOrderById,
+    getUserOrders,
+    getAllOrders,
+    updateOrderStatus,
+} = require("../controllers/orderController");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createOrder);
 router.get("/", authMiddleware, getUserOrders);
 router.get("/:id", authMiddleware, getOrderById);
+router.get("/admin/orders", authMiddleware, adminMiddleware, getAllOrders);
+router.put("/:id", authMiddleware, adminMiddleware, updateOrderStatus);
 
 module.exports = router;
